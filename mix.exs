66955e4881a8ef3d3a73aec9207b6b3a51967e16
Daniel Neighman <dneighman@gmail.com>
2018-10-13 21:30:38 -0700

    Adds license and changelog

--- mix.exs
+++ mix.exs
@@ -6,10 +6,10 @@ defmodule Abc.MixProject do
       app: :abc,
       version: "0.1.0",
       elixir: "~> 1.7",
-      license: "MIT",
-      description: "Message Broadcaster (GenStage + Swarm)",
+      description: description(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
     ]
   end
 
@@ -26,8 +26,22 @@ defmodule Abc.MixProject do
     [
       {:gen_stage, "~>0.14"},
       {:swarm, "~>3.3"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    "Event Broadcaster (GenStage + Swarm)"
+  end
+
+  defp package do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hassox/abc"}
     ]
   end
 end
